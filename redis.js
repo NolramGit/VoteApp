51ece887ded42bb55de8dce7550d548a4fbe432c
Marlon Amado <70726171+NolramGit@users.noreply.github.com>
2026-03-29 16:21:29 -0500

    new redis 2.0

--- redis.js
+++ redis.js
@@ -1,9 +1,9 @@
 const { createClient } = require('redis');
 
-const url = 'redis://default:roYFOjOFpohrNImlXBhbcSZSWLAgJbZB@redis.railway.internal:6379' || "";
+const url = 'redis://default:roYFOjOFpohrNImlXBhbcSZSWLAgJbZB@centerbeam.proxy.rlwy.net:46127' || "";
 
 const redisClient = createClient({
-    url: url
+    url: 'redis://default:roYFOjOFpohrNImlXBhbcSZSWLAgJbZB@centerbeam.proxy.rlwy.net:46127'
 });
 
 // redisClient.on('error', (err) => console.error('❌ Error en el Cliente de Redis:', err));
